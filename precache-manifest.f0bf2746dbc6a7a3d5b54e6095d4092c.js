c0bf20b60799f6ad3238a13e56b5ccea3311e0b5
Daniel <dsa.92cs@gmail.com>
2020-01-02 17:21:37 +0100

    Updates

---
+++ precache-manifest.f0bf2746dbc6a7a3d5b54e6095d4092c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c2563d67883b25e933865f88060804d",
+    "url": "/daninoan/index.html"
+  },
+  {
+    "revision": "5304a3a57c6b075f7fa9",
+    "url": "/daninoan/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0cffa3703e6d03809bb9",
+    "url": "/daninoan/static/js/2.446b3cb8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/daninoan/static/js/2.446b3cb8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5304a3a57c6b075f7fa9",
+    "url": "/daninoan/static/js/main.12f617a2.chunk.js"
+  },
+  {
+    "revision": "759d83f7069dd5dc214f",
+    "url": "/daninoan/static/js/runtime-main.2b2b2a41.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/daninoan/static/media/logo.5d5d9eef.svg"
+  }
+]);
