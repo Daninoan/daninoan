75e4580dcb8f52b20e46a4232147968c8f0c0a2a
Daniel <dsa.92cs@gmail.com>
2020-01-09 12:35:03 +0100

    Añadido logo de la universidad

--- src/components/experiencias.js
+++ src/components/experiencias.js
@@ -1,17 +1,24 @@
 import React, { Component } from "react";
-import { Card, CardHeader, Avatar, CardContent } from "@material-ui/core";
+import {
+  Card,
+  CardHeader,
+  Avatar,
+  CardContent,
+  Typography
+} from "@material-ui/core";
+import logoUji from "../assets/img/simbol-UJI-color.jpg";
 
 class Experiencias extends Component {
   render() {
     return (
       <Card>
         <CardHeader
-          avatar={<Avatar alt="UJI" src="/src/assets/img/logo.svg" />}
+          avatar={<Avatar alt="UJI" src={logoUji} />}
           title="Grado en estudios ingleses"
           subheader="Universitat Jaume I"
         />
         <CardContent>
-            <Typography variant="body2"></Typography>
+          <Typography variant="body2"></Typography>
         </CardContent>
       </Card>
     );
